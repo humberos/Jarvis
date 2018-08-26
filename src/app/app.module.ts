bc60a66768e61cf0703f5b86f8a917bb54abaa89
Humberto Borba <humberos@omnirom.org>
2018-08-26 07:45:43 -0300

    jarvis: One functional IONIC sample
    Signed-off-by: Humberto Borba <humberos@omnirom.org>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule} from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -13,6 +15,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -22,6 +25,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    InAppBrowser,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public feeds: Array<string>;
+    private url: string = "https://www.reddit.com/new.json";  
 
-  }
+    constructor(
+        public navCtrl: NavController,
+        public http: Http,
+        public loadingCtrl: LoadingController,
+        private inAppBrowser: InAppBrowser ) {
+        this.fetchContent();
+    }
 
+    fetchContent():void {
+        let loading = this.loadingCtrl.create({content: "Fetching content..."});
+        loading.present();
+        this.http.get(this.url).map(res => res.json()).subscribe(data => {
+            this.feeds = data.data.children;
+            loading.dismiss();
+        });
+    }
+
+    itemSelected (url: string):void {
+        this.inAppBrowser.create(url, "_self", "location=false");
+    }
 }
